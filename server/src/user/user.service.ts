a3873208ea04365c1e4b93beee5b3d75fdf3561e
Damir <dmustafic@datafin.me>
2022-07-01 15:25:50 +0200

    Reset password functionality to User entity

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "nestjs-prisma";
 import { UserServiceBase } from "./base/user.service.base";
 import { PasswordService } from "../auth/password.service";
+import { User } from "./base/User";
+import { UserFindUniqueArgs } from "./base/UserFindUniqueArgs";
 
 @Injectable()
 export class UserService extends UserServiceBase {
@@ -11,4 +13,24 @@ export class UserService extends UserServiceBase {
   ) {
     super(prisma, passwordService);
   }
+
+  async resetPassword(args: UserFindUniqueArgs): Promise<User> {
+    return this.prisma.user.update({
+      where: args.where,
+      data:{
+        password: generateRandomString(16)
+      }
+    });
+  }
 }
+
+function generateRandomString(length: number) {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,/{}=+-_)(*&^%$#@!~';
+  let charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
